a62741ecec58e77e2eae4d6bd77c4f339ef048f9
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-07-22 12:34:53 +0530

    Time: 3 ms (75.02%), Space: 20.5 MB (11.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    bool dfs(int i, vector<int>& visited, vector<int>& pathVis,  unordered_map<int, vector<int>> &mp) {
-        visited[i] = 1;
-        pathVis[i] = 1;
-        for(auto v : mp[i]){
-            if(!visited[v]){
-                if(dfs(v, visited, pathVis, mp)) return true;
-            }
-            else if(pathVis[v]){
-                return true;
-            }
-        }
-        pathVis[i] = 0;
-        return false;
-    }
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> degree(n, 0);
         unordered_map<int, vector<int>> mp;
-        for(auto &p : prerequisites){
+        for(auto &p : pre){
             mp[p[1]].push_back(p[0]);
+            degree[p[0]]++;
         }
-        vector<int> visited(n, 0);
-        vector<int> pathVis(n, 0);
-        for(int i=0; i<n; i++){
-            if(dfs(i, visited, pathVis, mp)){
-                return false;
+        vector<int> visited(n, false);
+        queue<int> q;
+        stack<int> st;
+        for(int i=0; i<n ;i++){
+            if(degree[i]==0){
+                q.push(i);
+                visited[i] = true;
+                st.push(i);
             }
         }
-        return true;
+        while(!q.empty()){
+            int s = q.size();
+            while(s--){
+                int f = q.front();
+                q.pop();
+                for(auto v : mp[f]){
+                    degree[v]--;
+                    if(degree[v]==0){
+                        q.push(v);
+                        st.push(v);
+                    }
+                }
+            }
+        }
+        return st.size()==n;
+
     }
 };
