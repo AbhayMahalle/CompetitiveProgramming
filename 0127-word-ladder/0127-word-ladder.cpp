8b7bb465dd4ff9fde6f760559407186fd014a265
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-01 00:31:34 +0530

    Time: 51 ms (76.28%), Space: 19.7 MB (93.91%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -5,25 +5,23 @@ public:
         int n = beginWord.size();
         queue<string> q;
         q.push(beginWord);
-        unordered_map<string, int> vis;
-        vis[beginWord] = 1;
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        st.erase(beginWord);
         while(!q.empty()){
             int s = q.size();
             while(s--){
                 string cur = q.front();
                 q.pop();
                 if(cur==endWord) return len;
-                for(auto w : wordList){
-                    if(vis.find(w)!=vis.end()){
-                        continue;
-                    }
-                    int diff = 0;
-                    for(int i=0; i<n; i++){
-                        if(w[i]!=cur[i]) diff++;
-                    }
-                    if(diff==1){
-                        vis[w] = 1;
-                        q.push(w);
+                string s = cur;
+                for(int i=0; i<n; i++){
+                    s = cur;
+                    for(char c='a'; c<='z'; c++){
+                        s[i] = c;
+                        if(st.find(s)!=st.end()){
+                            st.erase(s);
+                            q.push(s);
+                        }
                     }
                 }
             }
