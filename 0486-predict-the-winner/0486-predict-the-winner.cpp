70a36aceaf052cb0b1b2c0ff29d05dba339428b2
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-01 10:53:21 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int solve(int i, int j, int turn, int p1, int p2, vector<int>&nums){
-        if(i==j){
-            if(!turn){
-                p1 += nums[i];
-            }
-            else {
-                p2 += nums[i];
-            }
-            dp[i][j][turn] = p1-p2;
-            return p1-p2;
-        }
-        if(!turn){
-            dp[i][j][turn] = max(solve(i+1, j, !turn, p1+nums[i], p2, nums), solve(i, j-1, !turn, p1+nums[j], p2, nums));
-        }
-        else {
-            dp[i][j][turn] = min(solve(i+1, j, !turn, p1, p2+nums[i], nums), solve(i, j-1, !turn, p1, p2+nums[j], nums));
-        }
-        return dp[i][j][turn];
+    int solve(int i, int j, vector<int>&nums, vector<vector<int>>& dp){
+        if(i==j) return nums[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = max(nums[i]-solve(i+1, j, nums, dp), nums[j]-solve(i, j-1, nums, dp));
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        int res = solve(0, n-1, 0, 0, 0, nums);
-        return res>=0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(0, n-1, nums, dp)>=0;
     }
 };
