34a1b83b1400a19707c51461ce31caeba7b457fe
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-01 15:02:03 +0530

    Time: 11 ms (92.56%), Space: 11.7 MB (77.77%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -4,11 +4,8 @@ public:
         if(i==j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         int answer;
-        if(s[i]==s[j] || s[j]==s[j-1]){
+        if(s[i]==s[j]){
             answer = solve(i, j-1, s, dp);
-        }
-        else if(s[i]==s[i+1]){
-            answer = solve(i+1, j, s, dp);
         }
         else {
             answer = solve(i, j-1, s, dp) + 1;
@@ -20,7 +17,14 @@ public:
         }
         return dp[i][j] = answer;
     }
-    int strangePrinter(string s) {
+    int strangePrinter(string ss) {
+        string s = "";
+        s.push_back(ss[0]);
+        for(char c : ss){
+            if(c!=s.back()){
+                s.push_back(c);
+            }
+        }
         int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
         return solve(0, s.size()-1, s, dp);
