d6352aef1d2cf8b7b72614d2450a4d8db30ec92e
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-08 13:25:40 +0530

    Time: 158 ms (50.9%), Space: 88.7 MB (60.87%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    int solve(int i, int j, int p, vector<int>& stones, vector<vector<vector<int>>> &dp){
+    int solve(int i, int j, vector<int>& stones, vector<vector<int>> &dp){
         if(i>=j){
             return 0;
         }
-        if(dp[i][j][p]!=-1) return dp[i][j][p];
-        if(p){
-            int left = stones[j] - stones[i] + solve(i+1, j, !p, stones, dp);
-            int right = 0;
-            if(i>0) right = (stones[j-1] - stones[i-1]);
-            else right = stones[j-1];
-            right += solve(i, j-1, !p, stones, dp);
-            return dp[i][j][p] = max(left, right);
-        }
-        else {
-            int left = -(stones[j] - stones[i]) + solve(i+1, j, !p, stones, dp);
-            int right = 0;
-            if(i>0) right = -(stones[j-1] - stones[i-1]);
-            else right = -stones[j-1];
-            right += solve(i, j-1, !p, stones, dp);
-            return dp[i][j][p] = min(left, right);
-        }
-        return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left = stones[j] - stones[i] - solve(i+1, j, stones, dp);
+        int right = 0;
+        if(i>0) right = (stones[j-1] - stones[i-1]);
+        else right = stones[j-1];
+        right -= solve(i, j-1, stones, dp);
+        return dp[i][j] = max(left, right);
     }
     int stoneGameVII(vector<int>& stones) {
         int n = stones.size();
         for(int i=1; i<n; i++){
             stones[i] += stones[i-1];
         }
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         int i = 0, j = n-1;
-        return solve(i, j, 1, stones, dp);
+        return solve(i, j, stones, dp);
     }
 };
