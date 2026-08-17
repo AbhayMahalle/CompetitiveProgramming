720102f39b5c77ea99e5d8e1ea5b44d5d5a8df62
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-18 01:45:25 +0530

    Time: 260 ms (5.04%), Space: 158.9 MB (5.58%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int p, int n, vector<int>& stones){
+    vector<vector<int>> diff;
+    int solve(int i, int player, int n, vector<int>& prefixSum){
         if(i>=n){
+            // Out of Boundary!
             return 0;
         }
-        if(dp[i][p]!=-1) return dp[i][p];
-        if(p){
-            int take = stones[i] + solve(i+1, !p, n, stones);
+        if(diff[i][player]!=-1) return diff[i][player];
+        if(player){
+            // Alice is Trying to Maximize the score differece
+            int take = prefixSum[i] + solve(i+1, !player, n, prefixSum);
             int notTake = INT_MIN;
-            if(i+1<n) notTake = solve(i+1, p, n, stones);
-            dp[i][p] = max(take, notTake);
+            if(i+1<n) notTake = solve(i+1, player, n, prefixSum);
+            diff[i][player] = max(take, notTake);
         }
         else {
-            int take = -stones[i] + solve(i+1, !p, n, stones);
+            // Bob is Trying to Minimize the score differece
+            int take = -prefixSum[i] + solve(i+1, !player, n, prefixSum);
             int notTake = INT_MAX;
-            if(i+1<n) notTake = solve(i+1, p, n, stones);
-            dp[i][p] = min(take, notTake);
+            if(i+1<n) notTake = solve(i+1, player, n, prefixSum);
+            diff[i][player] = min(take, notTake);
         }
-        return dp[i][p];
+        return diff[i][player];
     }
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
+        vector<int> prefixSum = stones;
+        // Maintaining the prefixSum bcoz
+        // leftMost stones will be converted into 1
         for(int i=1; i<n; i++){
-            stones[i] += stones[i-1];
+            prefixSum[i] += prefixSum[i-1];
         }
-        dp.resize(n, vector<int>(2, -1));
-        return solve(1, 1, n, stones);
+        //Player 1 : Alice
+        //Player 0 : Bob
+        int player = 1;
+        int i = 1;
+        //Starting with 1 bcoz in each turn x>1
+        diff.resize(n, vector<int>(2, -1));
+        return solve(i, player, n, prefixSum);
     }
 };
