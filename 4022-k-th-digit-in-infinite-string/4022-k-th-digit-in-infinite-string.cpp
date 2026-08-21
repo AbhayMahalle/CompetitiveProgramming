2ea37f48f7006d313d0a3eb2f3884f80eed5a5a8
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-21 11:26:48 +0530

    Time: 0 ms (100%), Space: 9.3 MB (55.29%) - LeetHub

--- 4022-k-th-digit-in-infinite-string/4022-k-th-digit-in-infinite-string.cpp
+++ 4022-k-th-digit-in-infinite-string/4022-k-th-digit-in-infinite-string.cpp
@@ -4,26 +4,27 @@ public:
         long long d = 1;
         long long st = 1;
         long long total = 9;
-        while (k > total) {
+        while(k>total){
             k -= total;
             d++;
             st *= 10;
             total = 9LL * d * st;
         }
-        if (d == 1)
-            return k;
+        if(d==1) return k;
+        st = st/10;
         long long blockSize = 10 * d;
-        long long blockIndex = (k - 1) / blockSize;
-        long long b = st/10 + blockIndex;
-        long long pos = (k - 1) % blockSize;
+        long long blockIndex = (k-1) / blockSize;
+        long long b = st + blockIndex;
+        long long pos = (k-1) % blockSize;
         long long idx = pos / d;
-        long long digitPos = pos % d;
+        long long digit = pos % d;
         long long number;
-        if (b % 2 == 0)
+        if(b%2==0){
             number = 10 * b + idx;
-        else
+        }
+        else {
             number = 10 * b + 9 - idx;
-
-        return to_string(number)[digitPos]-'0';
+        }
+        return to_string(number)[digit]-'0';
     }
 };
