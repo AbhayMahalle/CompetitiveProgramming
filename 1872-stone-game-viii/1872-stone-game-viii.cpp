e7485a2fc14920004d313b7254cd81f4d5335a4d
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-08-24 14:25:58 +0530

    Time: 101 ms (74.82%), Space: 91.5 MB (76.43%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> diff;
-    int solve(int i, int player, int n, vector<int>& prefixSum){
-        if(i>=n){
-            // Out of Boundary!
-            return 0;
-        }
-        if(diff[i][player]!=-1) return diff[i][player];
-        if(player){
-            // Alice is Trying to Maximize the score differece
-            int take = prefixSum[i] + solve(i+1, !player, n, prefixSum);
-            int notTake = INT_MIN;
-            if(i+1<n) notTake = solve(i+1, player, n, prefixSum);
-            diff[i][player] = max(take, notTake);
-        }
-        else {
-            // Bob is Trying to Minimize the score differece
-            int take = -prefixSum[i] + solve(i+1, !player, n, prefixSum);
-            int notTake = INT_MAX;
-            if(i+1<n) notTake = solve(i+1, player, n, prefixSum);
-            diff[i][player] = min(take, notTake);
-        }
-        return diff[i][player];
-    }
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
-        vector<int> prefixSum = stones;
-        // Maintaining the prefixSum bcoz
-        // leftMost stones will be converted into 1
+        vector<int> prefix(n, 0);
+        prefix[0] = stones[0];
         for(int i=1; i<n; i++){
-            prefixSum[i] += prefixSum[i-1];
+            prefix[i] = stones[i] + prefix[i-1];
         }
-        //Player 1 : Alice
-        //Player 0 : Bob
-        int player = 1;
-        int i = 1;
-        //Starting with 1 bcoz in each turn x>1
-        diff.resize(n, vector<int>(2, -1));
-        return solve(i, player, n, prefixSum);
+        int best = prefix[n-1];
+        for(int i=n-2; i>=1; i--){
+            best = max(best, prefix[i]-best);
+        }
+        return best;
     }
 };
