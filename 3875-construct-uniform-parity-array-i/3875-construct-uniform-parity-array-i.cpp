387b2db89b83ac8334e38c37a769adbc39301eb0
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-01 23:38:35 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.55%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -2,34 +2,29 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
-        // try for all even
+        bool odd = true, even = true;
         for(int i=0; i<n; i++){
-            if(nums1[i]%2==0) nums1.push_back(nums1[i]);
-            else{
-                bool valid = false;
-                for(int j=0; j<n; j++){
-                    if(i!=j && abs(nums1[i]-nums1[j])%2==0){
-                        valid = true;
-                    }
-                }
-                if(!valid) break;
-            }
-            if(i==n-1) return true;
-        }
-        // try for all odd
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2==1) nums1.push_back(nums1[i]);
-            else{
+            if(nums1[i]%2==0){
+                // check for odd
                 bool valid = false;
                 for(int j=0; j<n; j++){
                     if(i!=j && abs(nums1[i]-nums1[j])%2==1){
                         valid = true;
                     }
                 }
-                if(!valid) break;
+                if(!valid) odd = false;
             }
-            if(i==n-1) return true;
+            else {
+                // check for even
+                bool valid = false;
+                for(int j=0; j<n; j++){
+                    if(i!=j && abs(nums1[i]-nums1[j])%2==0){
+                        valid = true;
+                    }
+                }
+                if(!valid) even = false;
+            }
         }
-        return false;
+        return odd | even;
     }
 };
