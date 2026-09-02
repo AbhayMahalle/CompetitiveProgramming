c31eb27b7842e458c3a00a278da640ac9448ed3b
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-02 22:18:00 +0530

    Time: 5 ms (55.18%), Space: 165.9 MB (63.21%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -10,29 +10,29 @@ public:
         int smallestOdd = INT_MAX;
 
         for(int num : nums1){
-            if(num % 2 == 0) smallestEven = min(smallestEven, num);
+            if(num%2==0) smallestEven = min(smallestEven, num);
             else smallestOdd = min(smallestOdd, num);
         }
 
-        if(smallestOdd == INT_MAX || smallestEven == INT_MAX) return true;
+        if(smallestOdd==INT_MAX || smallestEven==INT_MAX) return true;
 
-        for(int i = 0; i < n; i++){
-            if(nums1[i] % 2 == 0){
+        for(int i=0; i<n; i++){
+            if(nums1[i]%2==0){
                 // check for odd
-                if(!((nums1[i] - smallestOdd) >= 1 &&
-                     (nums1[i] - smallestOdd) % 2 == 1)){
+                if(!((nums1[i]-smallestOdd)>=1 &&
+                     (nums1[i]-smallestOdd)%2==1)){
                     canMakeOdd = false;
                 }
             }
             else {
                 // check for even
-                if(!((nums1[i] - smallestEven) >= 1 &&
-                     (nums1[i] - smallestEven) % 2 == 0)){
+                if(!((nums1[i]-smallestEven)>=1 &&
+                     (nums1[i]-smallestEven)%2==0)){
                     canMakeEven = false;
                 }
             }
+            if(canMakeEven==false && canMakeOdd==false) return false;
         }
-
-        return canMakeOdd || canMakeEven;
+        return true;
     }
 };
