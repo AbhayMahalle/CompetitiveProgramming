9188bde4deae8ac215eae0191a676085dbc642af
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-02 22:06:59 +0530

    Time: 12 ms (40.93%), Space: 165.8 MB (87.56%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
-        bool odd = true, even = true;
+
+        bool canMakeOdd = true;
+        bool canMakeEven = true;
+
         int smallestEven = INT_MAX;
         int smallestOdd = INT_MAX;
+
         for(int num : nums1){
-            if(num%2==0) smallestEven = min(smallestEven, num);
+            if(num % 2 == 0) smallestEven = min(smallestEven, num);
             else smallestOdd = min(smallestOdd, num);
         }
-        if(smallestOdd==INT_MAX || smallestEven==INT_MAX) return true;
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2==0){
+
+        if(smallestOdd == INT_MAX || smallestEven == INT_MAX) return true;
+
+        for(int i = 0; i < n; i++){
+            if(nums1[i] % 2 == 0){
                 // check for odd
-                if(!((nums1[i]-smallestOdd)>=1 && (nums1[i]-smallestOdd)%2==1)){
-                    odd = false;
+                if(!((nums1[i] - smallestOdd) >= 1 &&
+                     (nums1[i] - smallestOdd) % 2 == 1)){
+                    canMakeOdd = false;
                 }
             }
             else {
                 // check for even
-                if(!((nums1[i]-smallestEven)>=1 && (nums1[i]-smallestEven)%2==0)){
-                    even = false;
-                } 
+                if(!((nums1[i] - smallestEven) >= 1 &&
+                     (nums1[i] - smallestEven) % 2 == 0)){
+                    canMakeEven = false;
+                }
             }
         }
-        return odd | even;
+
+        return canMakeOdd || canMakeEven;
     }
 };
