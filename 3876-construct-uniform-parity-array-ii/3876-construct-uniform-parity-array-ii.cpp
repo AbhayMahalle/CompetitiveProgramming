a0fb3dd56c3303699c0884d116bc3cff53cf95dc
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-02 22:38:14 +0530

    Time: 3 ms (72.8%), Space: 165.9 MB (63.21%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-
-        bool canMakeOdd = true;
-        bool canMakeEven = true;
-
-        int smallestEven = INT_MAX;
+    bool uniformArray(vector<int>& nums) {
         int smallestOdd = INT_MAX;
 
-        for(int num : nums1){
-            if(num%2==0) smallestEven = min(smallestEven, num);
-            else smallestOdd = min(smallestOdd, num);
+        for (int num : nums) {
+            if (num % 2 == 1)
+                smallestOdd = min(smallestOdd, num);
         }
 
-        if(smallestOdd==INT_MAX || smallestEven==INT_MAX) return true;
+        // Already all even
+        if (smallestOdd == INT_MAX)
+            return true;
 
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2==0){
-                // check for odd
-                if(!((nums1[i]-smallestOdd)>=1 &&
-                     (nums1[i]-smallestOdd)%2==1)){
-                    canMakeOdd = false;
-                }
-            }
-            else {
-                // check for even
-                if(!((nums1[i]-smallestEven)>=1 &&
-                     (nums1[i]-smallestEven)%2==0)){
-                    canMakeEven = false;
-                }
-            }
-            if(canMakeEven==false && canMakeOdd==false) return false;
+        // Check whether every even number can become odd
+        for (int num : nums) {
+            if (num % 2 == 0 && num <= smallestOdd)
+                return false;
         }
+
         return true;
     }
 };
