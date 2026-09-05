f07302e1adc8aeb1398d5ba6bb5f5ba505e8417d
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-05 18:02:27 +0530

    Time: 481 ms (5%), Space: 125.2 MB (5.04%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int m, n;
-    bool solve(int i, int j, int mid, vector<vector<int>>& heights, vector<vector<int>>& vis) {
-        if(i == m-1 && j == n-1)
-            return true;
-        vis[i][j] = 1;
-        int dr[] = {-1,1,0,0};
-        int dc[] = {0,0,-1,1};
-        for(int k=0;k<4;k++){
-            int ni = i + dr[k];
-            int nj = j + dc[k];
-            if(ni>=0 && ni<m && nj>=0 && nj<n && !vis[ni][nj] && abs(heights[i][j]-heights[ni][nj])<=mid){
-                if(solve(ni,nj,mid,heights,vis)) return true;
-            }
-        }
-        return false;
-    }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        m = heights.size();
-        n = heights[0].size();
-        int l = 0, r = 1e6;
-        int res = r;
-        while(l<=r){
-            vector<vector<int>> visited(m, vector<int>(n, 0));
-            int mid = l + (r-l)/2;
-            if(solve(0, 0, mid, heights, visited)){
-                res = mid;
-                r = mid - 1;
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<vector<int>> absDiff(m, vector<int>(n, 1e9));
+        absDiff[0][0] = 0;
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {0, 0}});
+        int dr[] = {0, 1, 0, -1};
+        int dc[] = {1, 0, -1, 0};
+        while(!q.empty()){
+            auto t = q.front();
+            int lastDiff = t.first;
+            int i = t.second.first;
+            int j = t.second.second;
+            q.pop();
+            for(int k=0; k<4; k++){
+                int ni = i + dr[k];
+                int nj = j + dc[k];
+                if(ni<0 || nj <0 || ni>=m || nj>=n) continue;
+                int curDiff = max(abs(heights[i][j]-heights[ni][nj]), lastDiff);
+                if(curDiff < absDiff[ni][nj]){
+                    absDiff[ni][nj] = curDiff;
+                    q.push({curDiff, {ni, nj}});
+                }
             }
-            else l = mid + 1;
         }
-        return res;
+        return absDiff[m-1][n-1];
     }
-
 };
