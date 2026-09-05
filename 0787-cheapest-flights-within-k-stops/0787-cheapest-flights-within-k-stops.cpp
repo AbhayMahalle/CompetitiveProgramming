e1f627fbb11a4d6ac2b23337dc743f2a5eb70395
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-05 18:54:13 +0530

    Time: 4 ms (47.08%), Space: 17.4 MB (89.12%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>> dist(n, vector<int>(k+1, INT_MAX));
-        dist[src][0] = 0;
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+        priority_queue<
+                        tuple<int, int, int>, 
+                        vector<tuple<int, int, int>>, 
+                        greater<tuple<int, int, int>>
+                    > pq;
         pq.push({0, 0, src});
         while(!pq.empty()){
-            auto [dis, stop, node] = pq.top();
+            auto [stop, dis, node] = pq.top();
             pq.pop();
             for(auto &f : flights){
                 int u = f[0];
                 int v = f[1];
                 int w = f[2];
-                if(u==node && stop<=k && (dis + w) < dist[v][stop]){
-                    dist[v][stop] = dis + w;
-                    pq.push({dist[v][stop], stop+1, v});
+                if(u==node && stop<=k && (dis + w) < dist[v]){
+                    dist[v]= dis + w;
+                    pq.push({stop+1, dist[v], v});
                 }
             }
         }
-        int cheapest = *min_element(dist[dst].begin(), dist[dst].end());
+        int cheapest = dist[dst];
         return cheapest == INT_MAX ? -1 : cheapest;
     }
 };
