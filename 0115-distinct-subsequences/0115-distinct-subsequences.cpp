6fc111993b132f4c7d90e930bb48d15d849939bf
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-06 06:55:49 +0530

    Time: 11 ms (87.61%), Space: 8.8 MB (95.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int ways(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int take = 0;
-        if(s[i]==t[j]){
-            take = ways(i-1, j-1, s, t, dp);
-        }
-        int notTake = ways(i-1, j, s, t, dp);
-        return dp[i][j] = take + notTake;
-    }
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return ways(m-1, n-1, s, t, dp);
+        vector<int> dp (n+1, 0);
+        for(int i=1; i<=m; i++){
+            int last = 1;
+            for(int j=1; j<=n; j++){
+                int cur = dp[j];
+                int take = 0, notTake = 0;
+                if(s[i-1]==t[j-1]){
+                    take = last;
+                }
+                notTake = cur;
+                dp[j] = (take + notTake)%(int)(1e9+7);
+                last = cur;
+            }
+        }
+        return dp[n];
     }
 };
