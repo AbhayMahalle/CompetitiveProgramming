d165636b87f258507f7d6b8f0dab02c19d24feb6
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-07 06:03:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (78.81%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -3,14 +3,13 @@ public:
     int distinctSubseqII(string s) {
         int n = s.size();
         int MOD = 1e9+7;
-        vector<int> dp(n, 1);
         vector<int> countEndWith(26, 0);
         int sum = 0;
         for(int i=0; i<n; i++){
            int idx = s[i] - 'a';
-           dp[i] = (dp[i] + sum - countEndWith[idx] + MOD) % MOD;
-           sum = (sum + dp[i]) % MOD;
-           countEndWith[idx] = (countEndWith[idx] + dp[i]) % MOD;
+           int cur = (1 + sum - countEndWith[idx] + MOD) % MOD;
+           sum = (sum + cur) % MOD;
+           countEndWith[idx] = (countEndWith[idx] + cur) % MOD;
         }
         return sum;
     }
