8ebc6e5ec0525b52443fa2968cfa3a73c3bb595d
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-08 23:18:27 +0530

    Time: 0 ms (100%), Space: 9 MB (85.45%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -4,18 +4,9 @@ public:
         if(n<=999) return 0;
         long long cnt = 0;
         long long start = 1000;
-        long long end = 9999;
-        int digit = 4;
-        while(start<=1e15){
-            int mul = (digit-1)/3;
-            if(n>=start && n<=end){
-                cnt += mul*(n-start+1);
-                break;
-            }
-            cnt += 1LL*mul*(end-start+1);
-            start = start*10;
-            end = end*10+9;
-            digit++;
+        while(start<=n){
+            cnt += (n-start+1);
+            start *= 1000;
         }
         return cnt;
     }
