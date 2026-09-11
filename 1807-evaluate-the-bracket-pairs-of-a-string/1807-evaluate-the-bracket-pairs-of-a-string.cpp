e4d49d8c2181fcf62fc6c9cb34cfda7ae8ef1747
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-11 10:29:51 +0530

    Time: 119 ms (28.04%), Space: 147.5 MB (11.15%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -3,7 +3,6 @@ public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         string res = "";
         int n = s.size();
-        bool inBracket = false;
         unordered_map<string, string> mp;
         for(auto k : knowledge){
             mp[k[0]] = k[1];
@@ -11,24 +10,17 @@ public:
         string key = "";
         for(int i=0; i<n; i++){
             if(s[i]=='('){
-                inBracket = true;
-                continue;
+                i++;
+                while(s[i]!=')')
+                    key += s[i++];
             }
-            else if(s[i]==')'){
-                inBracket = false;
+            if(!key.empty()){
+                string cur = mp[key];
+                if(!cur.empty()) res.append(cur.begin(), cur.end());
+                else res += '?';
             }
-            if(inBracket){
-                key += s[i];
-            }
-            else{
-                if(!key.empty()){
-                    auto cur = mp[key];
-                    if(!cur.empty()) res.append(cur.begin(), cur.end());
-                    else res.push_back('?');
-                }
-                else res += s[i];
-                key = "";
-            }
+            else res += s[i];
+            key = "";
         }
         return res;
     }
