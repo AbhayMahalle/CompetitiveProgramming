e106411e054dbffdc955df85777d8d9d3ea0b523
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-12 12:15:07 +0530

    Time: 976 ms (47.82%), Space: 321.7 MB (46.74%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -7,25 +7,6 @@ public:
     vector<int> nxtIndex;
     vector<vector<P>> dp;
 
-    P solve(int i, int k){
-        if(k==0 || i>=intervals.size()) return {0, {}};
-        if(dp[i][k].first!=-1){
-            return dp[i][k];
-        }
-        P skip = solve(i+1, k);
-        auto &[l, r, w] = intervals[i];
-        P take = solve(nxtIndex[i], k-1);
-        take.second.push_back(originalIndex[intervals[i]]);
-        sort(take.second.begin(), take.second.end());
-        take.first += w;
-        if(take.first>skip.first){
-            return dp[i][k] = take;
-        }
-        else if(skip.first>take.first){
-            return dp[i][k] = skip;
-        }
-        return dp[i][k] = min(take, skip);
-    }
     vector<int> maximumWeight(vector<vector<int>>& a) {
         for(int i=0; i<a.size(); i++){
             if(originalIndex.find({a[i][0], a[i][1], a[i][2]})!=originalIndex.end()){
@@ -43,7 +24,27 @@ public:
             nxtIndex[i] = upper_bound(intervals.begin(), intervals.end(), 
                 make_tuple(r, INT_MAX, INT_MAX)) - intervals.begin();
         }
-        dp.resize(n, vector<P>(5, {-1, {}}));
-        return solve(0, 4).second;
+        dp.resize(n, vector<P>(5, {0, {}}));
+        for(int i=n-1; i>=0; i--){
+            for(int k=1; k<=4; k++){
+                P skip = {0, {}};
+                if(i+1<n) skip = dp[i+1][k];
+                auto &[l, r, w] = intervals[i];
+                int nxt = nxtIndex[i];
+                P take = {0, {}};
+                if(nxt<n) take = dp[nxt][k-1];
+                take.second.push_back(originalIndex[intervals[i]]);
+                sort(take.second.begin(), take.second.end());
+                take.first += w;
+                if(take.first>skip.first){
+                    dp[i][k] = take;
+                }
+                else if(skip.first>take.first){
+                    dp[i][k] = skip;
+                }
+                else dp[i][k] = min(take, skip);
+            }
+        }
+        return dp[0][4].second;
     }
 };
