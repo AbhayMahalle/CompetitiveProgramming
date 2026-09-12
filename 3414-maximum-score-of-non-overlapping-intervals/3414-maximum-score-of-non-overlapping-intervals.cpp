43ca0fa82a8dae7dc33414231cbb158ceb668c93
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-12 12:04:27 +0530

    Time: 2399 ms (17.39%), Space: 320.3 MB (46.74%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -2,48 +2,48 @@ class Solution {
 public:
     using P = pair<long long, vector<int>>;
 
+    map<tuple<int, int, int>, int> originalIndex;
+    vector<tuple<int, int, int>> intervals;
+    vector<int> nxtIndex;
+    vector<vector<P>> dp;
+
+    P solve(int i, int k){
+        if(k==0 || i>=intervals.size()) return {0, {}};
+        if(dp[i][k].first!=-1){
+            return dp[i][k];
+        }
+        P skip = solve(i+1, k);
+        auto [l, r, w] = intervals[i];
+        P take = solve(nxtIndex[i], k-1);
+        take.second.push_back(originalIndex[intervals[i]]);
+        sort(take.second.begin(), take.second.end());
+        take.first += w;
+        if(take.first>skip.first){
+            return dp[i][k] = take;
+        }
+        else if(skip.first>take.first){
+            return dp[i][k] = skip;
+        }
+        return min(take, skip);
+    }
     vector<int> maximumWeight(vector<vector<int>>& a) {
-        map<tuple<int,int,int>, int> originalIndex;
-        for (int i = 0; i < (int)a.size(); i++) {
-            if (originalIndex.find({a[i][0], a[i][1], a[i][2]}) != originalIndex.end())
+        for(int i=0; i<a.size(); i++){
+            if(originalIndex.find({a[i][0], a[i][1], a[i][2]})!=originalIndex.end()){
                 continue;
+            }
             originalIndex[{a[i][0], a[i][1], a[i][2]}] = i;
         }
-
-        vector<tuple<int,int,int>> intervals;
-        for (auto &[interval, index] : originalIndex)
+        for(auto [interval, index] : originalIndex){
             intervals.push_back(interval);
-
+        }
         int n = intervals.size();
-        vector<int> nextIndex(n);
-
-        for (int i = 0; i < n; i++) {
-            auto [left, right, weight] = intervals[i];
-            nextIndex[i] = upper_bound(
-                intervals.begin(),
-                intervals.end(),
-                make_tuple(right, INT_MAX, INT_MAX)
-            ) - intervals.begin();
+        nxtIndex.resize(n, n);
+        for(int i=0; i<n; i++){
+            auto [l, r, w] = intervals[i];
+            nxtIndex[i] = upper_bound(intervals.begin(), intervals.end(), 
+                make_tuple(r, INT_MAX, INT_MAX)) - intervals.begin();
         }
-
-        vector<vector<P>> dp(n + 1, vector<P>(5, {0, {}}));
-
-        for (int i = n - 1; i >= 0; i--) {
-            auto [left, right, weight] = intervals[i];
-            int idx = originalIndex[{left, right, weight}];
-
-            for (int k = 1; k <= 4; k++) {
-                P skip = dp[i + 1][k];
-
-                P take = dp[nextIndex[i]][k - 1];
-                take.first -= weight;
-                take.second.push_back(idx);
-                sort(take.second.begin(), take.second.end());
-
-                dp[i][k] = min(skip, take);
-            }
-        }
-
-        return dp[0][4].second;
+        dp.resize(n, vector<P>(5, {-1, {}}));
+        return solve(0, 4).second;
     }
 };
