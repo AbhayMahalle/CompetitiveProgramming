ffb53bc252eb9ce0dc4107ffa8f8617020b73fde
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-13 06:20:59 +0530

    Time: 35 ms (84.53%), Space: 15.8 MB (42.83%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
+
+        vector<pair<int, int>> ones1, ones2;
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (img1[r][c] == 1)
+                    ones1.push_back({r, c});
+                if (img2[r][c] == 1)
+                    ones2.push_back({r, c});
+            }
+        }
+
+        unordered_map<int, int> frequency;
         int maxOverlap = 0;
-        map<pair<int, int>, int> diff;
-        for(int r1=0; r1<n; r1++){
-            for(int c1=0; c1<n; c1++){
-                if(img1[r1][c1]==0) continue;
-                for(int r2=0; r2<n; r2++){
-                    for(int c2=0; c2<n; c2++){
-                        if(img2[r2][c2]==0) continue;
-                        int rDiff = r1-r2;
-                        int cDiff = c1-c2;
-                        diff[{rDiff, cDiff}] = 1;
-                    }
-                }
+
+        for (auto [r1, c1] : ones1) {
+            for (auto [r2, c2] : ones2) {
+                int dr = r1 - r2;
+                int dc = c1 - c2;
+                int key = dr * 100 + dc;
+                frequency[key]++;
+                maxOverlap = max(maxOverlap, frequency[key]);
             }
         }
-        for(auto &p : diff){
-            auto [rDiff, cDiff] = p.first; 
-            int overlap = 0;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
-                    int r = i + rDiff;
-                    int c = j + cDiff;
-                    if(r<0 || c<0 || r>=n || c>=n){
-                        continue;
-                    }
-                    if(img2[i][j]==img1[r][c] && img2[i][j]==1) overlap++;
-                }
-            }
-            maxOverlap = max(maxOverlap, overlap);
-        }
+
         return maxOverlap;
     }
 };
