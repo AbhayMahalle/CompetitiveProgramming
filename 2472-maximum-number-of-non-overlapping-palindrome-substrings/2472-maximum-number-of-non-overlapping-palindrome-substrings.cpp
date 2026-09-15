ba81c7fd323c1263bdac87a26a319e9ab92727a7
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-15 06:23:52 +0530

    Time: 11 ms (75.39%), Space: 8.9 MB (91.36%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-        int n = s.size();
-        if(k==1) return n;
-        vector<pair<int, int>> ranges(n, {-1, -1});
-        vector<bool> find(n, false);
-        for(int i=0; i<n; i++){
-            int l = i-1, r = i+1;
-            while(l>=0 && r<n){
-                if(s[l]==s[r]){
-                    if(r-l+1>=k){
-                        if(find[l]) break;
-                        ranges[l] = {l, r};
-                        find[l] = true;
+        int n = s.size(), ans = 0, end = -1;
+
+        for(int i = 0; i < n; i++){
+            for(int l : {i - 1, i}){
+                int r = i;
+                while (l >= 0 && r < n && s[l] == s[r]) {
+                    if (r - l + 1 >= k && l > end) {
+                        ans++;
+                        end = r;
                         break;
                     }
-                    l--;
-                    r++;
+                    l--, r++;
                 }
-                else break;
-            }
-            l = i, r = i+1;
-            while(l>=0 && r<n){
-                if(s[l]==s[r]){
-                    if(r-l+1>=k){
-                        if(find[l]) break;
-                        ranges[l] = {l, r};
-                        find[l] = true;
-                        break;
-                    }
-                    l--;
-                    r++;
-                }
-                else break;
             }
         }
-        int res = 0;
-        int i = 0;
-        while(i<n){
-            auto p = ranges[i];
-            int l = p.first, r = p.second;
-            if(l!=-1){
-                res++;
-                i = r+1;
-            }
-            else i++;
-        }
-        return res;
+        return ans;
     }
 };
