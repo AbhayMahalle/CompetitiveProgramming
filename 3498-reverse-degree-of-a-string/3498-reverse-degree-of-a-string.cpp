08b5fd4f37070f961f1379bf575efc0231118220
Abhay Mahalle <abhaymahalle0311@gmail.com>
2026-09-20 07:03:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int reverseDegree(string s) {
         int revDegree = 0, n = s.size();
         for(int i=0; i<n; i++){
-            revDegree += (i+1) * (27-(s[i]-'a'+1));
+            revDegree += (i+1) * (26-(s[i]-'a'));
         }
         return revDegree;
     }
